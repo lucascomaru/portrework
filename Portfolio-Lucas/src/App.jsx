2258a5fd7a1dd64d5ae0bc7ceecdbd4cddb26aa8
Lucas <lucascomaru20@gmail.com>
2023-07-12 19:16:43 -0300

    Fazendo ajustes

--- Portfolio-Lucas/src/App.jsx
+++ Portfolio-Lucas/src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css'
-import Inicio from './components/inicio'
-import Projetos from './components/projetos'
-
+import './App.css';
+import Inicio from './components/inicio';
+import Projetos from './components/projetos';
 
 function App() {
-
   return (
-    <>
-    <Inicio/>
-    <Projetos/>
-      
-    </>
-  )
+    <div className="app-container">
+      <div className="inicio-section">
+        <Inicio />
+      </div>
+      <div className="projetos-section">
+        <Projetos />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
